961fd3acb7378fef73fd393854beddff9ef92820
GuruDev0807 <stareast0807@gmail.com>
2024-02-29 05:08:00 -0500

    completed custom progress bart

--- components/ProgressBar/BridgeProgressBar.tsx
+++ components/ProgressBar/BridgeProgressBar.tsx
@@ -9,16 +9,30 @@ const BridgeProgressBar = ({ stepProgress }: any) => {
         <img src={"/assets/images/lineProgress.png"} width={"100%"} alt="" />
 
         <SectionStatsConnectWallet>
-          <SectionIconCurrent>
-            <SectionRotateRect></SectionRotateRect>
-            <IconCurrent>
-              <img
-                src={"/assets/images/icons/pinned.png"}
-                width={"100%"}
-                alt=""
-              />
-            </IconCurrent>
-          </SectionIconCurrent>
+          {stepProgress === 0 ? (
+            <SectionIconCurrent>
+              <SectionIconRotateRect></SectionIconRotateRect>
+              <IconCurrent>
+                <img
+                  src={"/assets/images/icons/pinned.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconCurrent>
+            </SectionIconCurrent>
+          ) : (
+            <SectionIconCurrent>
+              <SectionIconCircleBig></SectionIconCircleBig>
+              <IconProcessed>
+                <img
+                  src={"/assets/images/icons/checked.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconProcessed>
+            </SectionIconCurrent>
+          )}
+
           <SectionTextGroup>
             <TextSubject>Connect Your Wallet</TextSubject>
             <TextDescription>
@@ -28,16 +42,34 @@ const BridgeProgressBar = ({ stepProgress }: any) => {
         </SectionStatsConnectWallet>
 
         <SectionStatsSelectNFT>
-          <SectionIconCurrent>
-            <SectionRotateRect></SectionRotateRect>
-            <IconCurrent>
-              <img
-                src={"/assets/images/icons/pinned.png"}
-                width={"100%"}
-                alt=""
-              />
-            </IconCurrent>
-          </SectionIconCurrent>
+          {stepProgress === 0 ? (
+            <SectionIconCurrent>
+              <SectionIconCircleSmall></SectionIconCircleSmall>
+              <IconCurrent></IconCurrent>
+            </SectionIconCurrent>
+          ) : stepProgress === 1 ? (
+            <SectionIconCurrent>
+              <SectionIconRotateRect></SectionIconRotateRect>
+              <IconCurrent>
+                <img
+                  src={"/assets/images/icons/pinned.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconCurrent>
+            </SectionIconCurrent>
+          ) : (
+            <SectionIconCurrent>
+              <SectionIconCircleBig></SectionIconCircleBig>
+              <IconProcessed>
+                <img
+                  src={"/assets/images/icons/checked.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconProcessed>
+            </SectionIconCurrent>
+          )}
           <SectionTextGroup>
             <TextSubject>Select Your NFT’s</TextSubject>
             <TextDescription>
@@ -47,16 +79,34 @@ const BridgeProgressBar = ({ stepProgress }: any) => {
         </SectionStatsSelectNFT>
 
         <SectionTransferNFT>
-          <SectionIconCurrent>
-            <SectionRotateRect></SectionRotateRect>
-            <IconCurrent>
-              <img
-                src={"/assets/images/icons/pinned.png"}
-                width={"100%"}
-                alt=""
-              />
-            </IconCurrent>
-          </SectionIconCurrent>
+          {stepProgress === 2 ? (
+            <SectionIconCurrent>
+              <SectionIconRotateRect></SectionIconRotateRect>
+              <IconCurrent>
+                <img
+                  src={"/assets/images/icons/pinned.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconCurrent>
+            </SectionIconCurrent>
+          ) : stepProgress === 3 ? (
+            <SectionIconCurrent>
+              <SectionIconCircleBig></SectionIconCircleBig>
+              <IconProcessed>
+                <img
+                  src={"/assets/images/icons/checked.png"}
+                  width={"100%"}
+                  alt=""
+                />
+              </IconProcessed>
+            </SectionIconCurrent>
+          ) : (
+            <SectionIconCurrent>
+              <SectionIconCircleSmall></SectionIconCircleSmall>
+              <IconCurrent></IconCurrent>
+            </SectionIconCurrent>
+          )}
           <SectionTextGroup>
             <TextSubject>Transfer Your NFT’s</TextSubject>
             <TextDescription>
@@ -116,19 +166,58 @@ const SectionIconCurrent = styled(Box)`
   transform: translate(-50%, -50%);
 `;
 
-const SectionRotateRect = styled(Box)`
-  min-width: 44px;
-  height: 44px;
-  border-radius: 6px;
+const SectionIconRotateRect = styled(Box)`
+  min-width: 30px;
+  height: 30px;
+  border-radius: 4px;
   background-color: #00cfff;
   transform: rotate(-45deg);
+`;
+
+const SectionIconCircleBig = styled(Box)`
+  min-width: 24px;
+  height: 24px;
+  border-radius: 100%;
+  background-color: #00cfff;
+`;
+
+const SectionIconCircleSmall = styled(Box)`
+  width: 16px;
+  height: 16px;
+  box-shadow: 0px 0px 4px 5px rgba(0, 207, 255, 0.2);
+  border-radius: 100%;
+  background-color: #00cfff;
+
+  animation: shadow-animation 2s infinite;
+  @keyframes shadow-animation {
+    0% {
+      box-shadow: 0px 0px 3px 5px rgba(0, 207, 255, 0.3);
+    }
+    50% {
+      box-shadow: 0px 0px 3px 5px rgba(0, 207, 255, 0);
+    }
+    100% {
+      box-shadow: 0px 0px 3px 5px rgba(0, 207, 255, 0.3);
+    }
+  }
 `;
 
 const IconCurrent = styled(Box)`
   /* display: flex;
 
   transform: rotate(-45deg); */
-  width: 18px;
+  width: 14px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const IconProcessed = styled(Box)`
+  /* display: flex;
+
+  transform: rotate(-45deg); */
+  width: 12px;
   position: absolute;
   left: 50%;
   top: 50%;
